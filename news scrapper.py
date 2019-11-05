5bc6a1073e5e00b80f7cde7d390009791030f408
Dalton-Marhufu <53567083+Dalton-Marhufu@users.noreply.github.com>
2019-11-05 13:25:05 +0200

    Update news scrapper.py

--- news scrapper.py
+++ news scrapper.py
@@ -1,15 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-
+import ezgmail
 
 url = "https://ewn.co.za/"
 responce = requests.get(url)
 soup = BeautifulSoup(responce.text, 'html.parser')
 news = soup.find('h4')
 
+#To view output in console
 for news in soup.find_all("h4"):
-    print(news.text) #To view output in console
+    print(news.text) 
 
+#Loop through news headlines and create a txt file
 with open('mynews.txt', 'w') as f:
     for news in soup.find_all("h4"):
        f.write((news.text + '\n'))
+       
+#create an email attachment and send
+ezgmail.send('daltonmarhufu@gmail.com', 'Todays-News!', 'Beep bop this is a bot giving news', ['mynews.txt'])
